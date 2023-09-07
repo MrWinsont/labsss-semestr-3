e27269b5a84efce930dac07f7917b4cd3b304fe9
unknown <aleksandrgrin263@gmail.com>
2023-09-07 20:57:36 +0200

    +lab1

---
+++ lab1/L1.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <stack>
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	std::stack <char> mas;
+	std::string str;
+	std::cout << "Введите строку - ";
+	std::cin >> str;
+	bool error = 0;
+	if (str[0] == '(' || str[0] == '[' || str[0] == '{')
+		mas.push(str[0]);
+	else {
+		std::cout << "строка не существует";
+	}
+	for (size_t i = 1; i < str.length(); i++)
+	{
+		if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
+			mas.push(str[i]);
+		}
+		else if ((str[i] == ')' && mas.top() == '(') || (str[i] == ']' && mas.top() == '[') || (str[i] == '}' && mas.top() == '{')) {
+			mas.pop();
+		}
+		else {
+			std::cout << "строка не существует" << "\n";
+			error = 1;
+			break;
+		}
+	}
+	if (!error)
+		std::cout << "строка существует" << "\n";
+
+	//()[({}())]
+}
