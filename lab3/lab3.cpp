1d09280a436afa5ec6ac3d17b8d32a431b3a25a8
MrWinsont <aleksandrgrin263@gmail.com>
2023-09-28 19:51:21 +0200

    +lab3

---
+++ lab3/lab3.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+#include<cmath>
+
+int main()
+{
+    double x;
+    std::cin >> x;
+    for (int i = 0; i < log(x)/log(3); i++) {
+        for (int j = 0; j < log(x) / log(5); j++) {
+            for (int k = 0; k < log(x) / log(7); k++) {
+                if (pow(3, i) * pow(5, j) * pow(7, k)<=x) {
+                    std::cout << pow(3, i) * pow(5, j) * pow(7, k)<<"\n";
+                }
+            }
+        }
+
+    }
+        
+}
+
