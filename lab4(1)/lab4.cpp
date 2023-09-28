6928c955fd23d7004e3c9f3a394c716f4c7bc3b5
MrWinsont <aleksandrgrin263@gmail.com>
2023-09-28 19:51:50 +0200

    +lab4(1)

---
+++ lab4(1)/lab4.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    double factor = 1.247;
+    std::vector<int> arr{ 5, 1, 4, 2, 8, 255, 64, 8, 74, 9, 6, 7, 5, 6, 2, 5, 8, 6 };
+    //std::vector<int> arr{ 1,-1 };
+
+    double step = arr.size();
+    while (step > 1) {
+        step /= factor;
+        for (int i = 0; i + step < arr.size(); i++) {
+            if (arr[i] > arr[i + step]) {
+                std::swap(arr[i], arr[i + step]);
+            }
+        }
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << "\n";
+    }
+}
